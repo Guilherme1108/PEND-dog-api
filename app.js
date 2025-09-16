333e4b12fc44ea04c19e2b382b888b4017d5b202
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-09-16 08:38:46 -0300

    se não digitar nenhuma raça, ele busca 50 imagens de cachorros aleatorios agora

--- app.js
+++ app.js
@@ -4,33 +4,40 @@
 const buttonTrocarRaca = document.getElementById('pesquisarRaca')
 
 function mudarRaca() {
-    const nomeRaca = document.getElementById('nomeRaca').value
-    return nomeRaca
+  const nomeRaca = document.getElementById('nomeRaca').value
+  return nomeRaca
 }
 buttonTrocarRaca.addEventListener('click', async () => {
-    const nomeRaca = mudarRaca()
-    const imagens = await buscarImagens(nomeRaca)
-    criarImagens(imagens)
-  })
-  
+  const nomeRaca = mudarRaca()
+  const imagens = await buscarImagens(nomeRaca)
+  criarImagens(imagens)
+})
+
 
 
 async function buscarImagens(nomeRaca) {
-    const url = `https://dog.ceo/api/breed/${nomeRaca}/images`
+
+let url
+
+  if (!nomeRaca) {
+    url = 'https://dog.ceo/api/breeds/image/random/50'
+  } else {
+    url = `https://dog.ceo/api/breed/${nomeRaca}/images`
+  }
     const response = await fetch(url)
     const imagens = await response.json()
     return imagens.message
+
 }
 
 function criarImagens(listaDeImagens) {
-    const conjuntoImagens = document.getElementById('divImagens')
-    conjuntoImagens.innerHTML = '' // limpa as imagens
-  
-    listaDeImagens.forEach(url => {
-      const imagemCachorro = document.createElement('img')
-      imagemCachorro.classList.add('dogImg')
-      imagemCachorro.src = url
-      conjuntoImagens.appendChild(imagemCachorro)
-    })
-  }
-  
+  const conjuntoImagens = document.getElementById('divImagens')
+  conjuntoImagens.innerHTML = '' // limpa as imagens
+
+  listaDeImagens.forEach(url => {
+    const imagemCachorro = document.createElement('img')
+    imagemCachorro.classList.add('dogImg')
+    imagemCachorro.src = url
+    conjuntoImagens.appendChild(imagemCachorro)
+  })
+}
